19ea73d2b6edf3c5481526ae68e64af0b4e6390f
Batuhan YILMAZ <100535577+batuhanylmz1421@users.noreply.github.com>
2022-03-20 21:15:28 +0300

    week 3

--- InsuranceManagementSystem/account/AccountManager.java
+++ InsuranceManagementSystem/account/AccountManager.java
@@ -6,18 +6,18 @@ public class AccountManager {
     private static TreeSet<Individual> accountsIndividual = new TreeSet<>(new AccountComparator());
     private static TreeSet<Enterprise> accountsEnterprise = new TreeSet<>(new AccountComparator());
 
-    public static void logIn(String email, String password, int choice) {
+    public static void login(String email, String password, int choice) {
         if (choice == 1) {
             for (Individual account : accountsIndividual) {
                 try {
                     account.login(email, password);
                 } catch (InvalidAuthenticationException e) {
                     if (email.equals(account.getUser().getEmail()) && !password.equals(account.getUser().getPassword())) {
-                        System.out.println("The email exists in the system. However, password is wrong. Please, enter password again.");
+                        System.out.println("The email or password wrong, Try again..");
                     } else {
                         System.out.println();
                         System.out.println("Such an account does not exist in the system.");
-                        System.out.println("If you don't have an account, you can sign up.");
+                        System.out.println("If you don't have an account, you need to sign up.");
                     }
                 }
             }
@@ -30,15 +30,15 @@ public class AccountManager {
                         System.out.println("The email exists in the system. However, password is wrong. Please, enter password again.");
                     } else {
                         System.out.println();
-                        System.out.println("Such an account does not exist in the system.");
-                        System.out.println("If you don't have an account, you can sign up.");
+                        System.out.println("Account not found.");
+                        System.out.println("If you don't have an account, you need to sign up.");
                     }
                 }
             }
         }
         if (accountsIndividual.isEmpty() && accountsEnterprise.isEmpty()) {
-            System.out.println("Such an account does not exist in the system.");
-            System.out.println("If you don't have an account, you can sign up.");
+            System.out.println("Account not found.");
+            System.out.println("If you don't have an account, you need to sign up.");
         }
     }
 
